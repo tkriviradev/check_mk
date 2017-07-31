699a7cf3684f103f864ccebfce670df8eec067f1
tkriviradev <tkriviradev@gmail.com>
2017-07-31 15:28:43 +0300

    Update ad_replication.bat

--- ad_replication.bat
+++ ad_replication.bat
@@ -1,1 +1,21 @@
+@echo off
 
+REM ***
+REM * To be able to run this check you need appropriate credentials
+REM * in the target domain.
+REM *
+REM * Normally the Check_MK agent runs as sevice with local system
+REM * credentials which are not enough for this check.
+REM *
+REM * To solve this problem you can do e.g. the following:
+REM *
+REM * - Change the account the service is being started with to a
+REM *   domain user account with enough permissions on the DC.
+REM *
+REM ***
+
+echo ^<^<^<ad_replication^>^>^>
+dsquery server | find /I "CN=%COMPUTERNAME%," > nul
+if ERRORLEVEL 1 goto SERVER_NOT_IN_DC_LIST
+repadmin /showrepl /csv
+:SERVER_NOT_IN_DC_LIST
